86a5d156aff342afeab8547650b9e97861c0f909
sharandakoleksii <85165521+sharandakoleksii@users.noreply.github.com>
2021-07-23 15:11:03 +0300

    Add files via upload

---
+++ Task2Script.bash
@@ -0,0 +1,11 @@
+#!/bin/sh
+$ < /dev/urandom tr -dc "[:alnum:]" | head -c1000 > random_text_$NUM.txt
+$ < /dev/urandom tr -dc "[:alnum:]" | head -c1000 > random_text_$NUM.txt
+$ < /dev/urandom tr -dc "[:alnum:]" | head -c1000 > random_text_$NUM.txt
+$ < /dev/urandom tr -dc "[:alnum:]" | head -c1000 > random_text_$NUM.txt
+$ < /dev/urandom tr -dc "[:alnum:]" | head -c1000 > random_text_$NUM.txt
+$ < /dev/urandom tr -dc "[:alnum:]" | head -c1000 > random_text_$NUM.txt
+$ < /dev/urandom tr -dc "[:alnum:]" | head -c1000 > random_text_$NUM.txt
+$ < /dev/urandom tr -dc "[:alnum:]" | head -c1000 > random_text_$NUM.txt
+$ < /dev/urandom tr -dc "[:alnum:]" | head -c1000 > random_text_$NUM.txt
+$ < /dev/urandom tr -dc "[:alnum:]" | head -c1000 > random_text_$NUM.txt
